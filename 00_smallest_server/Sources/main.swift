d54ef32708a459c194d0c9a075c3c83719c6be30
carlynorama <carlynorama@gmail.com>
2025-09-01 07:44:25 -0700

    add trouble-shootng notes for now

--- 00_smallest_server/Sources/main.swift
+++ 00_smallest_server/Sources/main.swift
@@ -9,6 +9,22 @@
 // // run hummingbird application
 // try await app.runService()
 
+// Things tried to track down the issue: 
+// - Add / route? [NO]
+// - Add something with a response body? [NO]
+// - Change from main.swift to App.swift @main { static main() } [NO]
+// - Moved HelloServer Source into this package's source. [YES!!]
+//     - remove / route [not it]
+//     - remove response body [not it]
+//     - remove specialized context [not it]
+//     - remove logging [not it]
+//     - remove Foundation [not it]
+//     - remove separate router call [not it]
+//     - fold build app builder back into App.swift [not it]
+//     - remove configuration [YES THAT'S IT! ]
+// - Add Argument Parser? 
+
+
 
 import Hummingbird
 // create router and add a single GET /hello route
